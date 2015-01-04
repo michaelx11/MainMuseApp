9b5c8bc4b6df8d04539630e300a0fae8a5a9ee59
Michael Xu <michaelx@michaels-air.attlocal.net>
2015-01-04 16:30:17 -0600

    First Version with FB Login

---
+++ MainMuse/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  MainMuse
+//
+//  Created by Michael Xu on 1/3/15.
+//  Copyright (c) 2015 Michael Xu. All rights reserved.
+//
+
+#ifndef MainMuse_Bridging_Header_h
+#define MainMuse_Bridging_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
--- MainMuse/ViewController.swift
+++ MainMuse/ViewController.swift
@@ -8,12 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBLoginViewDelegate {
+    
+    @IBOutlet var fbLoginView : FBLoginView!;
+    @IBOutlet var textView : UITextView!;
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.fbLoginView.delegate = self;
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"];
     }
+    
+    // Facebook Delegate Methods
+    
+    func loginViewShowingLoggedInUser(loginView: FBLoginView!) {
+        println("User Logged In");
+    }
+    
+    func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+        println("User: \(user)");
+        println("User ID: \(user.objectID)");
+        println("User Name: \(user.name)");
+        var userEmail = user.objectForKey("email") as String;
+        println("User Email: \(userEmail)");
+        self.textView.text = "User Name: \(user.name)";
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
+        println("User Logged Out");
+    }
+    
+    func loginView(loginView: FBLoginView!, handleError: NSError) {
+        println("Error: \(handleError.localizedDescription)");
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
